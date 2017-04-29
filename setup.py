a7a82f48fba08fcafab9e9216062e138c2c18b81
Ian Day <iandday@gmail.com>
2017-04-29 14:43:57 -0400

    working hub detection!

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requirements = [x.strip() for x in open(here('requirements.txt')).readlines()]
 
 setup(
     name='pyharmony',
-    version='1.0.14',
+    version='1.0.13',
     description='Python library for programmatically using a Logitech Harmony Link or Ultimate Hub.',
     long_description=readme,
     author='Ian Day',
